bfec54741175698796a5b5292c3bba314fbd3a5b
Luis <lvtamez@gmail.com>
2018-09-22 16:44:33 -0400

    X

---
+++ Lab2/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Products
+        public ActionResult Details(string ProductName)
+        {
+            ViewBag.ProductMessage = "";
+            if(!string.IsNullOrWhiteSpace(ProductName))
+            ViewBag.ProductMessage = "Your selected product: "+ProductName;
+            return View();
+        }
+    }
+}
